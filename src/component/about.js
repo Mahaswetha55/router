70b88ae4d090d4e355b98127e16866dbd0a8c500
Mahaswetha55 <mahaswethachitra@gmail.com>
2024-01-08 14:40:30 +0530

    first commit

---
+++ src/component/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+ 
+function About() {
+    return (
+        <div>
+            <h2>
+            React Router is a standard library for routing in React!!!...
+            </h2>
+            Read more about us at :
+            <a href="https://www.React Router.org/about/">
+                https://www.React Router.org/about/
+            </a>
+        </div>
+    );
+}
+export default About;
---
+++ src/component/contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+ 
+function Contact() {
+    return (
+        <address>
+            You can find us here:
+            <br />
+            React Router
+            <br />
+            5th & 9th Floor, Royal Street, A- 1111, <br />
+            Sector- 136, chennai, TamilNadu (201305)
+        </address>
+    );
+}
+ 
+export default Contact;
